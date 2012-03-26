ce2373316b048c51b3016e9d3c936fd6bd25d8a2
Andrew Crump <andrew@kotirisoftware.com>
2012-03-26 14:59:02 +0100

    FC001: Use strings in preference to symbols.

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,7 +21,7 @@ include_recipe "apt"
 
 template "/etc/apt/sources.list" do
   mode 0644
-  variables :code_name => node[:lsb][:codename]
+  variables :code_name => node['lsb']['codename']
   notifies :run, resources(:execute => "apt-get update"), :immediately
   source "sources.list.erb"
 end
