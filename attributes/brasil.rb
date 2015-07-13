79765fe34f55b204f8182a373abff2bce2c6f407
Roberto Cunha <robertosilvino@gmail.com>
2015-07-13 01:38:14 -0300

    ajuste de erro de sintaxe

--- attributes/brasil.rb
+++ attributes/brasil.rb
@@ -1,4 +1,2 @@
 node.normal['ubuntu']['archive_url']  = 'http://ubuntu.c3sl.ufpr.br/ubuntu/'
 node.normal['ubuntu']['security_url'] = 'http://ubuntu.c3sl.ufpr.br/ubuntu/'
-
-include_recipe 'ubuntu::default'
